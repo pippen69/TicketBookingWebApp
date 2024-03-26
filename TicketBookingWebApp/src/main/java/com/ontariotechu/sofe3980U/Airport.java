8968ba91929ae915fc7856fde2161d8db4033487
Javier Chung <34190746+Javiebear@users.noreply.github.com>
2024-03-25 22:00:22 -0400

    Add files via upload

---
+++ TicketBookingWebApp/src/main/java/com/ontariotechu/sofe3980U/Airport.java
@@ -0,0 +1,16 @@
+package com.ontariotechu.sofe3980U;
+import java.lang.*;
+import java.util.*;
+
+public class Airport {
+    String airportName;
+    Map<String, String> airportDistAssocArr = new Hashmap <String, String>;
+    String[] directFlight;
+    
+    //int getFlightTime(){}
+
+    //void generateDirectFlights(){};
+
+    //void listDirectFlights(){}
+
+}
